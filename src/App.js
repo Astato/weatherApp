1ce8c2cea8398dc48ca83a72a5d617c8e16ea0b8
Astato <leivagfernando@outlook.com>
2024-06-22 05:05:49 -0300

    Update and minor improvements - Refator: UI when no location is available. - Update: Packages updated. - Refactor: Added query fetch and postMessage to be implemented with parent container app.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import './style.css'
+import "./style.css";
 // import ReactDOM from 'react-dom/client';
-import React, { useEffect, useState } from 'react';
-import WeatherCards from './Weathercard';
+import React, { useEffect, useState } from "react";
+import WeatherCards from "./Weathercard";
 // import Forecast from './Forecast';
 
-
 const NavBar = () => {
-  return(
-    <footer className='navbar'>
+  return (
+    <footer className="navbar">
       <h1>WeatherWise</h1>
       {/* <div id='searchfield'>
         <label htmlFor='citysearch'>Search: </label>
         <input id='citysearch' type='text' placeholder='New York'></input>
       </div> */}
-      <a href='https://openweathermap.org/' target='_blank' rel='noreferrer'>Powered by OpenWeather</a>
+      <a href="https://openweathermap.org/" target="_blank" rel="noreferrer">
+        Powered by OpenWeather
+      </a>
     </footer>
-  )
-}
+  );
+};
 
 function App() {
-  const [appClass, setAppClass] = useState('')
-  
-  useEffect(() => {
+  const [appClass, setAppClass] = useState("");
+  const [userPosition, setUserPosition] = useState({});
 
-  },[appClass])
+  useEffect(() => {}, [appClass]);
+
+  useEffect(() => {
+    const query = window.location.search;
+    const params = new URLSearchParams(query);
+    const latitude = params.get("latitude");
+    const longitude = params.get("longitude");
+    setUserPosition({ latitude: latitude, longitude: longitude });
+  }, []);
 
   return (
-    <div id='App' className={appClass}>
-      <div id='weather-cards-container'>
-        <WeatherCards  appclass={setAppClass} ></WeatherCards>
+    <div id="App" className={appClass}>
+      <div id="weather-cards-container">
+        <WeatherCards
+          appclass={setAppClass}
+          userPosition={userPosition}
+        ></WeatherCards>
       </div>
       <NavBar></NavBar>
     </div>
--- src/Forecast.js
+++ src/Forecast.js
@@ -1,100 +1,117 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
 const currentDay = new Date().getUTCDate();
 let upcomingForecast = [];
 let today = [];
 
-
-export function mapForecast(tempUnit, forecast){
-  if(upcomingForecast.length>1 && today.length>1){
+export function mapForecast(tempUnit, forecast) {
+  if (upcomingForecast.length > 1 && today.length > 1) {
     return;
   }
-  const forecastList = forecast.map(item => {
-    const {dt_txt} = {...item};
-    const forecastTime = dt_txt.split(' ')[1].slice(0,5)
-    const forecastDay = dt_txt.split(' ')[0].split('-')[2];
-    let {feels_like} = {...item.main};
-    if(tempUnit === '°F') {
-      feels_like = (((feels_like -273.15)*9/5+32).toFixed());
+  const forecastList = forecast.map((item) => {
+    const { dt_txt } = { ...item };
+    const forecastTime = dt_txt.split(" ")[1].slice(0, 5);
+    const forecastDay = dt_txt.split(" ")[0].split("-")[2];
+    let { feels_like } = { ...item.main };
+    if (tempUnit === "°F") {
+      feels_like = (((feels_like - 273.15) * 9) / 5 + 32).toFixed();
     }
-    if(tempUnit === '°C') {
-      feels_like = (feels_like -273.15).toFixed();
+    if (tempUnit === "°C") {
+      feels_like = (feels_like - 273.15).toFixed();
     }
-    const {main, icon} = {...item.weather[0]}
+    const { main, icon } = { ...item.weather[0] };
     const iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
-    if (Number(forecastDay) > currentDay ) {
-      upcomingForecast.push([forecastDay,iconURL, main, feels_like, forecastTime, dt_txt]);
+    if (Number(forecastDay) > currentDay) {
+      upcomingForecast.push([
+        forecastDay,
+        iconURL,
+        main,
+        feels_like,
+        forecastTime,
+        dt_txt,
+      ]);
     }
-    if(Number(forecastDay) <= currentDay+1) {
-      today.push([forecastDay,iconURL, main, feels_like, forecastTime, dt_txt]);
+    if (Number(forecastDay) <= currentDay + 1) {
+      today.push([
+        forecastDay,
+        iconURL,
+        main,
+        feels_like,
+        forecastTime,
+        dt_txt,
+      ]);
     }
-    return (upcomingForecast, today)
+    return upcomingForecast, today;
   });
   return forecastList;
-
 }
 
-
-
 const Forecast = (props) => {
-const [forecast, setForecast] = useState('');
-const latitude = (props.locationLat).toString();
-const longitude = (props.locationLon).toString();
-const tempUnit = props.units;
+  const [forecast, setForecast] = useState("");
+  const latitude = props.locationLat.toString();
+  const longitude = props.locationLon.toString();
+  const tempUnit = props.units;
 
-
-useEffect(() => {
+  useEffect(() => {
     async function getForecast() {
-      const rawForecast = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&appid=58ab3e4fa033b9933bab777dff79301b" , {mode:'cors'})
-      const forecastData = await rawForecast.json()
-      return setForecast(forecastData)
-    
+      const rawForecast = await fetch(
+        "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+          latitude +
+          "&lon=" +
+          longitude +
+          "&appid=58ab3e4fa033b9933bab777dff79301b",
+        { mode: "cors" }
+      );
+      const forecastData = await rawForecast.json();
+      return setForecast(forecastData);
     }
     today = [];
     upcomingForecast = [];
     getForecast();
-  },[latitude, longitude, tempUnit])
-
+  }, [latitude, longitude, tempUnit]);
 
   useEffect(() => {
-    if(today.length>1) {
-      props.setToday(today)
+    if (today.length > 1) {
+      props.setToday(today);
     }
-  })
+  });
 
-  if(forecast) {
-    const {list} = {...forecast}
+  if (forecast) {
+    const { list } = { ...forecast };
 
-    mapForecast(tempUnit, list)
+    mapForecast(tempUnit, list);
 
-    function handleClick(day){
-      const expandElements = document.getElementsByClassName(day)
-      const show = 'display:flex; flex-direction:column;'
-      Array.from(expandElements).forEach(element => {
-        switch(element.style.display) {
-          case '':
-          case 'none': element.setAttribute('style',show) ; return;
-          case 'flex': element.style.display = 'none';return;
-          default: element.style.display = 'none;'
+    function handleClick(day) {
+      const expandElements = document.getElementsByClassName(day);
+      const show = "display:flex; flex-direction:column;";
+      Array.from(expandElements).forEach((element) => {
+        switch (element.style.display) {
+          case "":
+          case "none":
+            element.setAttribute("style", show);
+            return;
+          case "flex":
+            element.style.display = "none";
+            return;
+          default:
+            element.style.display = "none;";
         }
-      })
+      });
     }
-
-
 
     function getMinMaxTemp(day) {
       let maxtemp = -10000;
       let minTemp = 10000;
-      for(const item of upcomingForecast) {
-        if(Number(item[3]) > Number(maxtemp) && item[0] === day){
-          maxtemp = Number(item[3])
+      for (const item of upcomingForecast) {
+        if (Number(item[3]) > Number(maxtemp) && item[0] === day) {
+          maxtemp = Number(item[3]);
         }
-        if(Number(item[3]) < minTemp && item[0] === day) {
-          minTemp = Number(item[3])
+        if (Number(item[3]) < minTemp && item[0] === day) {
+          minTemp = Number(item[3]);
         }
       }
-      return ([maxtemp," / ", minTemp])
+      return [maxtemp, " / ", minTemp];
     }
 
     let dayAdded = false;
@@ -104,87 +121,82 @@ useEffect(() => {
       const iconURL = item[1];
       const temperature = item[3];
       const forecastTime = item[4];
-   
 
-      if(!dayDate !== item[0]) {
-        dayDate = item[0]
+      if (!dayDate !== item[0]) {
+        dayDate = item[0];
       }
       /// after the tomorrow day has been added, check if the current element is same as previous, if not, set dayAdded to false, to add the next day
       // example: previous date = 17 (tomorrow) current date 18.  .
-      if (index > 0 && upcomingForecast[index][0]  !== upcomingForecast[index-1][0]){
+      if (
+        index > 0 &&
+        upcomingForecast[index][0] !== upcomingForecast[index - 1][0]
+      ) {
         dayAdded = false;
       }
-      
-   
+
       let extended;
-      ///// Tomorrow day if no day has not been added yet 
-      if (!dayAdded && !day){
-        day =
-        <div key={index} id='day' onClick={() => handleClick(item[0])}>
-          <p>Tomorrow</p>
-          <div style= {{display: 'flex'}} >
-            <img src={iconURL} alt={item[2]}></img>
-            <p>{getMinMaxTemp(item[0])}°</p>
-          </div>
-      </div>;
-      /// add the first item before the return otherwise the first item is missing
-        extended =
-        <div key={index+1} id='extended' className={item[0]}>
-            <p>{temperature}°</p>
-            <img src={iconURL} alt={item[2]}></img>
-            <p>{forecastTime}</p>
-          </div>
-      dayAdded = true;
-      return([day,extended])
-    }
-    if((item[0] === dayDate) && !dayAdded) {
-      
-      const forecastDay = new Date(item[5]).toDateString().split(' ')[0];
-      // averageTemperature = (averageTemperature / addedTemperatures).toFixed();
-      day =
-      <div key={index} id='day' onClick={() => handleClick(item[0])}>
-            <p>{forecastDay}</p>
-            <div style= {{display: 'flex'}} >
+      ///// Tomorrow day if no day has not been added yet
+      if (!dayAdded && !day) {
+        day = (
+          <div key={index} id="day" onClick={() => handleClick(item[0])}>
+            <p>Tomorrow</p>
+            <div style={{ display: "flex" }}>
               <img src={iconURL} alt={item[2]}></img>
               <p>{getMinMaxTemp(item[0])}°</p>
             </div>
           </div>
-          /// same principle as the first day,extended return 
-        extended =
-        <div key={index+1} id='extended' className={item[0]}>
+        );
+        /// add the first item before the return otherwise the first item is missing
+        extended = (
+          <div key={index + 1} id="extended" className={item[0]}>
             <p>{temperature}°</p>
             <img src={iconURL} alt={item[2]}></img>
             <p>{forecastTime}</p>
           </div>
+        );
         dayAdded = true;
-        return ([day,extended]);
+        return [day, extended];
+      }
+      if (item[0] === dayDate && !dayAdded) {
+        const forecastDay = new Date(item[5]).toDateString().split(" ")[0];
+        // averageTemperature = (averageTemperature / addedTemperatures).toFixed();
+        day = (
+          <div key={index} id="day" onClick={() => handleClick(item[0])}>
+            <p>{forecastDay}</p>
+            <div style={{ display: "flex" }}>
+              <img src={iconURL} alt={item[2]}></img>
+              <p>{getMinMaxTemp(item[0])}°</p>
+            </div>
+          </div>
+        );
+        /// same principle as the first day,extended return
+        extended = (
+          <div key={index + 1} id="extended" className={item[0]}>
+            <p>{temperature}°</p>
+            <img src={iconURL} alt={item[2]}></img>
+            <p>{forecastTime}</p>
+          </div>
+        );
+        dayAdded = true;
+        return [day, extended];
       }
       /// continure the iteration to add the remaining items of the current day once the day was added to the component.
       if (dayDate === item[0]) {
-        extended =
-        <div key={index} id='extended' className={item[0]}>
+        extended = (
+          <div key={index} id="extended" className={item[0]}>
             <p>{temperature}°</p>
             <img src={iconURL} alt={item[2]}></img>
             <p>{forecastTime}</p>
           </div>
-        return  extended;
+        );
+        return extended;
       }
-      
-      return [day, extended]
-      })
 
+      return [day, extended];
+    });
 
-  return(
-    <div id='forecast'>
-      {extendedForecast}
-    </div>
-  )
-
+    return <div id="forecast">{extendedForecast}</div>;
   }
+};
 
-}
-
-
-
-
-export default Forecast;;
+export default Forecast;
